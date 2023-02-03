3fb044da3fd7fb91abc360afbda1072fda659093
Robert Patrick Jordan Sazon <rpjsazon@gmail.com>
2023-02-03 16:27:46 +0000

    locate device

--- assets/js/leaflet-map.js
+++ assets/js/leaflet-map.js
@@ -1,9 +1,36 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+// Displaying map - Manual Input
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+// L.marker([51.5, -0.09]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//     .openPopup();
+
+// Reference: https://leafletjs.com/examples/mobile/example.html - Showing Current Location
+const map = L.map('map').fitWorld();
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+
+function onLocationFound(e) {
+	const radius = e.accuracy / 2;
+
+    const locationMarker = L.marker(e.latlng).addTo(map)
+	.bindPopup(`You are within ${radius} meters from this point`).openPopup();
+
+	const locationCircle = L.circle(e.latlng, radius).addTo(map);
+}
+
+function onLocationError(e) {
+	alert(e.message);
+}
+
+map.on('locationfound', onLocationFound);
+map.on('locationerror', onLocationError);
+map.locate({setView: true, maxZoom: 16});
