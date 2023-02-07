32244c1e3dc90e193b82891bcaf0178baf6e9911
Ralph Cox <73188137+essexgit@users.noreply.github.com>
2023-02-07 16:25:54 +0000

    overwriting error

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// Alert to show input of search //
+const searchButton = document.getElementById('button-search');
+const searchInput = document.getElementById('textbox-search');
+searchButton.addEventListener('click', () => {
+    const uniName = searchInput.value;
+    alert(uniName);
+});
+//
+$.getJSON(
+    "http://universities.hipolabs.com/search?name=${uniName}",
+    function (data) {
+        console.log(data);
+
+    }
+);
+
+//
+let uniQuery = `http://universities.hipolabs.com/search?name=${uniName}`;
+//search by university - offer Unis by name to select
+async function hipoUniAjax() {
+    await $.ajax({
+        url: uniQuery,
+        method: 'GET'
+    }).then(function (response) {
+        for (let i = 0; i < response.length; i++) {
+            let nameTown = response[i].name;
+            let dns = response[i].domains[0];
+            console.log("dns", dns);
+            console.log("data", nameTown);
+        }
+
+
+        // return data;
+    });
+}
+hipoUniAjax();
