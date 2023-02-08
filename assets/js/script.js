117e8e7b3f746b4931f9b28e9c1ab68bb54a2959
Robert Patrick Jordan Sazon <rpjsazon@gmail.com>
2023-02-08 13:37:41 +0000

    added script code for searching town and university by @ralph and @ryan.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,40 @@
+let UniArrayToChoose = [];
 let uniQuery = "";
 // Alert to show input of search //
 const searchButton = $('#button-search');
 const searchInput = $('#textbox-search');
 const searchForm = $('#searchForm');
+const uniList = $('#uniList');
+const dropdown = $('#dropdown');
+$(document).ready(function () {
+    uniList.on('click', 'option', function () {
+        let dns = $(this).val();
+        console.log("dns");
+    });
+});
 searchForm.on("submit", function (event) {
     event.preventDefault();
     let uniName = searchInput.val();
-    console.log("uniName", uniName);
-    // uniQuery = `http://universities.hipolabs.com/search?name=${uniName}`;
-
-    // return uniQuery;
+    uniQuery = `http://universities.hipolabs.com/search?name=${uniName}`;
+    hipoUniAjax(uniQuery);
 });
-
-// hipoUniAjax(uniQuery)
-//
-// $.getJSON(
-//     "http://universities.hipolabs.com/search?name=${uniName}",
-//     function (data) {
-//         console.log(data);
-
-//     }
-// );
-
-//
-
-
-;
 // search by university - offer Unis by name to select;
-// async function hipoUniAjax() {
-//     await $.ajax({
-//         url: uniQuery,
-//         method: 'GET'
-//     }).then(function (response) {
-//         for (let i = 0; i < response.length; i++) {
-//             let nameTown = response[i].name;
-//             console.log("data", nameTown);        // return data;
-//         }
-//     });
-// }
+async function hipoUniAjax(uniQuery) {
+    await $.ajax({
+        url: uniQuery,
+        method: 'GET'
+    }).then(function (response) {
+        for (let i = 0; i < response.length; i++) {
+            let nameTown = response[i].name;
+            UniArrayToChoose.push(nameTown);
+        }
+        generateDropdown(UniArrayToChoose);
+    });
+}
+function generateDropdown(array) {
+    // loop through array creating options
+    array.forEach(element => {
+        let createDropdown = $(`<option >${element}</option>`);
+        uniList.append(createDropdown);
+    });
+}
