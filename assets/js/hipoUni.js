598ab8c52192e336239b0860834681927e5ca22b
RyzaT <ryantaylor50@hotmail.com>
2023-02-08 12:30:59 +0000

    moved previous edited script.js into hipo.js
    This is to avoid conflict with script.js on main

--- assets/js/hipoUni.js
+++ assets/js/hipoUni.js
@@ -1,26 +1,37 @@
-let uniName = "kent";
+// Alert to show input of search //
+const searchButton = document.getElementById('button-search');
+const searchInput = document.getElementById('textbox-search');
+searchButton.addEventListener('click', () => {
+  const uniName = searchInput.value;
+  alert(uniName);
+});
+//
+$.getJSON(
+    "http://universities.hipolabs.com/search?name=${uniName}",
+    function(data) {
+        console.log(data);
+        
+    }
+);
 
+//
 let uniQuery = `http://universities.hipolabs.com/search?name=${uniName}`;
 //search by university - offer Unis by name to select
-function hipoUniAjax() {
-    $.ajax({
+async function hipoUniAjax() {
+    await $.ajax({
         url: uniQuery,
         method: 'GET'
     }).then(function (response) {
         for (let i = 0; i < response.length; i++) {
-            let data = response[i].name;
-            console.log("data", data);
+            let nameTown = response[i].name;
+            let dns = response[i].domains[0];
+            console.log("dns", dns);
+            console.log("data", nameTown);
         }
 
 
-        return data;
+        // return data;
     });
 }
 hipoUniAjax();
-//extract domain
-//add domain into IP
-// longitude 1 mile = 0.0182
-// latitude 1 mile = 0.0145
-// add lat & long, Min & max to Open trip
-// show map in iframe
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -33,4 +33,3 @@ async function hipoUniAjax() {
         // return data;
     });
 }
-hipoUniAjax();
