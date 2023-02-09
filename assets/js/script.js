1616a300d9fa69a8345ea46e6e148d70d34c7aa2
RyzaT <ryantaylor50@hotmail.com>
2023-02-09 10:18:35 +0000

    Updated Screenshot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 let UniArrayToChoose = [];
 let uniQuery = "";
-​
+
 // Alert to show input of search //
 const searchButton = $('#button-search');
 const searchInput = $('#textbox-search');
@@ -8,15 +8,14 @@ const searchForm = $('#searchForm');
 const uniList = $('#uniList');
 const dropdown = $('#dropdown');
 const uniArray = [];
-​
+
 searchForm.on("submit", function (event) {
     event.preventDefault();
     let uniName = searchInput.val();
     uniQuery = `https://cors-anywhere.herokuapp.com/http://universities.hipolabs.com/search?name=${uniName}`;
-​
     hipoUniAjax(uniQuery);
 });
-​
+
 // search by university - offer Unis by name to select;
 async function hipoUniAjax(uniQuery) {
     await $.ajax({
@@ -32,7 +31,7 @@ async function hipoUniAjax(uniQuery) {
         // releaseUniArray(UniArrayToChoose);
     });
 }
-​
+
 function generateDropdown(array) {
     // loop through array creating options
     array.forEach(element => {
@@ -40,13 +39,13 @@ function generateDropdown(array) {
         uniList.append(createDropdown);
     });
 }
-​
-​
+
+
 // function passDNS(string) {
 //     console.log(string);
 // }
 // -----------------mapping section-----------------------------
-​
+
 let IP = "";
 const geo = {};
 const latLong = [];
@@ -55,18 +54,18 @@ const mapOptions = {
     zoom: 15
 };
 let radius = 1600;
-​
+
 const ipGeoApiKey = "5f9bbb64079640a4bd6561979f9b3747";
 let ipGeoApiQuery = "";
 const apiKeyTrip = "5ae2e3f221c38a28845f05b6e0ffe54a717dac1df5bd2ef8183fe293";
-​
+
 // let tripQuery = "";
-​
+
 //use dns query to get IP
 let dataFromDns = async (url) => {
     await Promise.resolve($.getJSON(url)).then(response => {
         // console.log("response", response);
-​
+
         IP = response.records.A[0].address;
         ipGeoApiQuery = `https://api.ipgeolocation.io/ipgeo?apiKey=${ipGeoApiKey}&ip=${IP}`;
     });
@@ -78,17 +77,17 @@ let geoFromIp = async (ipGeoApiQuery) => {
         geo.longitude = parseFloat(response.longitude);
     });
 };
-​
+
 let getLatFromGeo = (geo) => {
     latitude = geo.latitude;
     return latitude;
 };
-​
+
 let getLongFromGeo = (geo) => {
     longitude = geo.longitude;
     return longitude;
 };
-​
+
 let getLatLongArrayFromGeo = (geo) => {
     getLatFromGeo(geo);
     getLongFromGeo(geo);
@@ -98,7 +97,7 @@ let getLatLongArrayFromGeo = (geo) => {
     // console.log("latLong", latLong);
     return latLong;
 };
-​
+
 let createMapOptions = (latLong) => {
     // console.log("latLong0", latLong[0]);
     // console.log("latLong1", latLong[1]);
@@ -106,14 +105,14 @@ let createMapOptions = (latLong) => {
     // console.log("mapOptions", mapOptions);
     return mapOptions;
 };
-​
+
 let createTripQuery = (radius, latitude, longitude, apiKeyTrip) => {
     // console.log("longitude", longitude);
     // console.log("latitude", latitude);
     return `https://api.opentripmap.com/0.1/en/places/radius?radius=${radius}&lat=${latitude}&lon=${longitude}&kinds=churches,banks,bars&format=geojson&apikey=${apiKeyTrip}`;
 };
-​
-​
+
+
 async function geoDataToMapFin(dns) {
     //dns from clicked choice
     let aQueryIP = `https://networkcalc.com/api/dns/lookup/${dns}`;
@@ -124,12 +123,12 @@ async function geoDataToMapFin(dns) {
     const tripQuery = createTripQuery(radius, latitude, longitude, apiKeyTrip);
     console.log("end of geo data");
     let map = new L.map('map', mapOptions);
-​
+
     let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
     map.addLayer(layer);
-​
+
     let marker = L.marker(latLong);
-​
+
     marker.addTo(map);
     markerMaker(tripQuery, map);
 }
@@ -146,12 +145,12 @@ $("select")
             let dns = await uniArray[UniPosition].domains[0];
             geoDataToMapFin(dns);
         })();
-​
+
     });
 //
-​
+
 // local information layers
-​
+
 function writeTriplayer(place) {
     let tripLayer = {
         placeName: place.properties.name,
@@ -160,14 +159,14 @@ function writeTriplayer(place) {
     };
     return tripLayer;
 };
-​
+
 let markerMaker = async (tripQuery, map) => {
-​
+
     let response = await $.ajax({
         url: tripQuery,
         method: 'GET'
     });
-​
+
     // console.log("tripQuery", tripQuery);
     // console.log("response", response);
     let places = response.features;
@@ -175,16 +174,16 @@ let markerMaker = async (tripQuery, map) => {
         createMarkers(map, places[i]);
     }
 };
-​
+
 function createMarkers(theMap, place) {
     let tripOMG = writeTriplayer(place);
     // console.log("place", place);
     // console.log("tripOMG", tripOMG);
-​
+
     // Leaflet.js Markercode 
     let place1 = L.marker([tripOMG.placeLat, tripOMG.placeLong], { title: tripOMG.placeName });
     // console.log("place1", place1);
-​
+
     place1.addTo(theMap);
     // console.log("map", theMap);
     place1.bindPopup(`<b>${tripOMG.placeName}</b><br>`).openPopup();
